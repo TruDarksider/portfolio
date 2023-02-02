e54f08ab4c84ff2a91324bd98d7920061ee5e9aa
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-02-02 15:42:11 -0600

    Restructered stylesheets to be in separate folder for easier styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import './stylesheets/App.css';
+import Bio from './components/Bio'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Bio />
       </header>
     </div>
   );
