1317ed73bc59480667cb2d16e2e71772e5b8caaa
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-02-02 16:55:32 -0600

    Created skeleton of footer but put in real info lol

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import '../stylesheets/Footer.css'
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <h1>Like what you see?</h1>
+            <p>Let's talk about working together!</p>
+            <span>406.696.7640</span><br/>
+            <span>tyler.hopkin12@gmail.com</span><br/><br/>
+            <span>Pflugerville, TX</span><br/>
+            <span>But willing to relocate, especially to Portland or Seattle.</span>
+        </div>
+    )
+}
+
+export default Footer;
