5fcf699ee7a79c5c2cedc9e751db6ea8f27a075d
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-02-06 12:40:22 -0600

    Picked 4 of the 6 projects to display, but did not link. Still debating between linking to another live git page or bringing it all into one folder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,23 +14,23 @@ function Projects(){
                 </div>
                 <div className='project-card'>
                     <img alt='' src={warlock}/>
-                    <h2>Project 2</h2>
-                    <p>This is a short description. It's just a couple of sentences</p>
+                    <h2>Photo Tagging App</h2>
+                    <p>Find the target in the picture. Uses Firebase as a backend for holding the answerkey.</p>
                 </div>
                 <div className='project-card'>
                     <img alt='' src={warlock}/>
-                    <h2>Project 3</h2>
-                    <p>This is a short description. It's just a couple of sentences</p>
+                    <h2>Etch-A-Sketch</h2>
+                    <p>Use your mouse to mimic a classic toy inside a browser window.</p>
                 </div>
                 <div className='project-card'>
                     <img alt='' src={warlock}/>
-                    <h2>Project 4</h2>
-                    <p>This is a short description. It's just a couple of sentences</p>
+                    <h2>Knight Travails</h2>
+                    <p>Find the shortest route for a knight to reach a particular spot on a chessboard. Uses a graph data structure to plot the course.</p>
                 </div>
                 <div className='project-card'>
                     <img alt='' src={warlock}/>
-                    <h2>Project 5</h2>
-                    <p>This is a short description. It's just a couple of sentences</p>
+                    <h2>Shop</h2>
+                    <p>Pretend to go shopping for made up items.</p>
                 </div>
                 <div className='project-card'>
                     <img alt='' src={warlock}/>
