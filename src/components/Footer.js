e467bd9791ae6129cb6b4b765d85805d536637cd
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-06-07 14:35:05 -0700

    Updated styling for smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,24 @@
 import '../stylesheets/Footer.css'
 
 function Footer(){
-    return(
-        <div className='footer' id='contact'>
-            <h1>Like what you see?</h1>
-            <p>Let's talk about working together!</p>
-            <span>406.696.7640</span><br/>
-            <span>tyler.hopkin12@gmail.com</span><br/><br/>
-            <span>Ashland, OR</span><br/>
-            <span>But willing to relocate within the Pacific Northwest.</span>
-        </div>
-    )
+    return (
+      <div className="footer" id="contact">
+        <h1>Like what you see?</h1>
+        <p>Let's talk about working together!</p>
+        <span>406.696.7640</span>
+        <br />
+        <span>tyler.hopkin12@gmail.com</span>
+        <br />
+        <br />
+        <span>Ashland, OR</span>
+        <br />
+        <span id="bottom">
+          But willing to relocate within the Pacific Northwest.
+        </span>
+            <br />
+        <span id="bottom"></span>
+      </div>
+    );
 }
 
 export default Footer;
