59736e1edce8383e0ad6698fd5e47d00ff976acc
Tyler <tyler.hopkin12@gmail.com>
2024-07-16 17:01:41 -0700

    Updated address and removed reduntant text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,12 @@ import '../stylesheets/Footer.css'
 function Footer(){
     return (
       <div className="footer" id="contact">
-        <h1>Like what you see?</h1>
-        <p>Let's talk about working together!</p>
+        <h1>Let's talk about working together!</h1>
         <span>406.696.7640</span>
         <br />
         <span>tyler.hopkin12@gmail.com</span>
         <br />
-        <br />
-        <span>Ashland, OR</span>
-        <br />
-        <span>
-          Willing to relocate within the Pacific Northwest.
-        </span>
+        <span>Seattle, WA</span>
         <br />
         <br />
         <span id="bottom">Warlock class icon from Destiny 2, a game made by BUNGiE. Github and LinkedIn icons from their samely titled companies.</span>
