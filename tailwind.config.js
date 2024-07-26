e321d5e1fe7625b297c8cdb3819bce9ee9255f31
Tyler <tyler.hopkin12@gmail.com>
2024-07-25 17:07:52 -0700

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      "my-red": "#AA1155",
+      "my-orange": "#D45113",
+      "my-green": "#94C9A9",
+      "my-blue": "#2C666E",
+      "my-white": "#FFFFFF",
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
