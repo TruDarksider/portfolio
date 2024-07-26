ef5f1a8ee1b2c185fc01dfbd29999435b6696075
Tyler <tyler.hopkin12@gmail.com>
2024-07-25 17:10:46 -0700

    Updated with new colors from tailwind config

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,12 +14,12 @@ function Header() {
           <a className="link" href='#contact'>Contact</a>
         </div>
       </div> */
-      <div className="flex sticky bg-white place-content-evenly text-2xl z-10">
+      <div className="flex sticky bg-my-white place-content-evenly text-2xl z-10">
         <div className="flex items-center border-solid">
           <img className="h-12 w-auto" alt="Warlock symbol from Destiny 2 which Tyler frequently plays" src={warlock} />
           <div className="hidden sm:block">Tyler Hopkin</div>
         </div>
-        <div className="grid grid-flow-col place-content-evenly items-center gap-6">
+        <div className="text-my-blue grid grid-flow-col place-content-evenly items-center gap-6">
           <a href='#about'>About Me</a>
           <a href='#projects'>Projects</a>
           <a href='#contact'>Contact</a>
