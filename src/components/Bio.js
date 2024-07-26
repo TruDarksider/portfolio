434423f732c867bea9b226d0d0e841602fe8f859
Tyler <tyler.hopkin12@gmail.com>
2024-07-25 17:11:04 -0700

    Roughly converted to Tailwind, mobile only

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,11 +6,7 @@ import headshot from '../images/editedHeadshot.png'
 function Bio(){
 
     return (
-      <div className="bio" id="about">
-        {/* <div className='nameAndPic'>
-                <h1>Tyler Hopkin</h1>
-                <img alt='Headshot of Tyler Hopkin' src={warlock} />
-            </div> */}
+      /*<div className="bio" id="about">
         <img
           alt="Headshot of Tyler Hopkin"
           src={headshot}
@@ -52,6 +48,52 @@ function Bio(){
                   alt="Destiny 2 Warlock class icon linking to Tyler's Destiny 2 character"
                   src={warlock}
                   className="externalLink"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>*/
+      <div className="pt-16 flex items-center bg-my-blue text-xl sm:flex-col gap-2">
+        <img
+          alt="Headshot of Tyler Hopkin"
+          src={headshot}
+          className="bg-my-green rounded-2xl shadow-md shadow-my-orange"
+        />
+        <div className="text-center text-my-white px-6">
+          <h2 className='text-2xl drop-shadow-md py-4'>Hey! I'm Tyler.</h2>
+          <p className='pb-4'>
+            I'm a software engineer specializing in web and app development. As a former audio engineer, I discovered a love for energizing an audience; now I create fun digital experiences either that bring even more people joy through personal projects and others' open source ones.
+          </p>
+          <p>
+            I enjoy learning new things and challenging myself. While I'm currently working with Javascript and React, I've previously played around in C++, Python, Unreal Engine, and Wwise.
+          </p>
+          
+          <ul className="flex justify-center gap-4 py-4">
+            <li>
+              <a href="https://github.com/TruDarksider">
+                <img
+                  alt="GitHub logo linking to Tyler's github profile"
+                  src={github}
+                  className="h-10 w-auto"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/tylerhopkin/">
+                <img
+                  alt="LinkedIn logo linking to Tyler's LinkedIn profile"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+                  className="h-10 w-auto"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.bungie.net/7/en/User/Profile/3/4611686018485783402?bgn=TruDarksider">
+                <img
+                  alt="Destiny 2 Warlock class icon linking to Tyler's Destiny 2 character"
+                  src={warlock}
+                  className="h-10 w-auto"
                 />
               </a>
             </li>
