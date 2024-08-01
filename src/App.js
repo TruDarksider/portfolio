d0617f4d079e44ba304a9057942a9b3419612248
Tyler <tyler.hopkin12@gmail.com>
2024-08-01 16:48:58 -0700

    Refactored to support react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Header from './components/Header'
 import Bio from './components/Bio'
 import Projects from './components/Projects';
 import Footer from './components/Footer'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div className="flex-col"> {/* classname="App" */}
+    <BrowserRouter>
       <Header />  
-      <Bio />
-      <Projects />
+      <Routes>  
+        <Route path='about/' exact element={<Bio />} />
+        <Route path='/' element={<Projects />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
