91fcde455a4d34b4d3f97a5194de8f25d69ec363
Tyler <tyler.hopkin12@gmail.com>
2024-08-01 16:51:48 -0700

    Changed bg color and removed some text about links that are obsolete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,13 +33,12 @@ function Projects() {
         </div>
         {isToggled ? <Carousel/> : <DynamicCards/>}
       </div>*/
-      <div className="flex-col text-center text-md p-6">
-        <h1 className='text-2xl py-4'>Projects</h1>
-        <p>
-          Title is a link to a live deployment, github image links to code. All
-          links open in a new tab.
+      <div className="pt-16 flex-col text-center text-md p-6 bg-my-blue">
+        <h1 className='text-2xl py-4 text-my-white'>Projects</h1>
+        <p className='text-my-white'>
+          All links open in a new tab.
         </p>
-        <div className='text-right text-sm md:pr-60'>
+        <div className='text-right text-sm md:pr-60 text-my-white'>
           {'Carousel view '}
             {isToggled ? <button className='bg-my-green rounded-2xl p-1' onClick={handleToggle}>ON</button> 
               : <button className='bg-my-red rounded-2xl p-1' onClick={handleToggle}>OFF</button>}
