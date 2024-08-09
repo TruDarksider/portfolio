ff64f4fcd05ea4d85bc84f56043cc5f38564dcd2
Tyler <tyler.hopkin12@gmail.com>
2024-08-09 16:20:09 -0700

    Attempt 3 now with HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Bio from './components/Bio'
 import Projects from './components/Projects';
 import Footer from './components/Footer'
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={'/'}>
+    <HashRouter>
       <Header />  
       <Routes>  
         <Route path='/about' exact element={<Bio />} />
         <Route path='/' element={<Projects />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
