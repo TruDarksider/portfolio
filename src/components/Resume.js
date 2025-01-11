f49b95de08465d360388ec70f343e2d9b98c5066
Tyler <tyler.hopkin12@gmail.com>
2025-01-11 14:39:58 -0800

    styling update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,19 +2,26 @@ import workHistory from '../work.json'
 
 const Resume = () => {
 return(
-    <div className="pt-16 bg-my-red text-my-white min-h-dvh">
-        <h1>Work History</h1>
-        <div>
-            {workHistory.map((job)=>(
-                <div key={job.id}>
-                    <h2>{job.title} - 
-                        <p>{job.location}</p>
-                    </h2>
-                    <h3>{job.company}</h3>
-                    <p>{job.dates}</p>
-                    <p>{job.desc}</p>
-                </div>
-            ))}
+    <div className="flex flex-col items-center pt-16 px-16 bg-my-red text-my-white min-h-dvh">
+        <div className='max-w-7xl'>
+            <h1 className='text-center'>Work History</h1>
+            <div className='flex flex-col gap-6'>
+                {workHistory.map((job)=>(
+                    <div key={job.id}>
+                        <div>
+                            <div className='flex justify-between'>
+                                <span>{job.title}</span>
+                                <span>{job.location}</span>
+                            </div>
+                            <div className='flex justify-between'> 
+                                <span>{job.company}</span>
+                                <span>{job.dates}</span>
+                            </div>
+                        </div>
+                        <p>{job.desc}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     </div>
 )
